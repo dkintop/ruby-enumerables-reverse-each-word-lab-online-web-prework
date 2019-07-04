741aa7e065ef891a68ca43d19a12f88845bbfd95
Doug Kintop <dkintop@hotmail.com>
2019-07-04 01:26:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   string_array = string.split
-  result = string_array.each do |element|
-    element.reverse
+  result = string_array.each do |word| 
+  word.reverse
   end
   result
-  
 end 
