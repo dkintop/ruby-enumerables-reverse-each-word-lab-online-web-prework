146090e49517e96a3fbbe030e6458ef37c66faf7
Doug Kintop <dkintop@hotmail.com>
2019-07-04 01:50:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,14 @@ def reverse_each_word(string)
    b
 end 
 
+def reverse_each_word(string)
+  new_array= []
+  string_array = string.split
+  
+   string_array.collect do |word| 
+    a =  word.reverse
+    new_array << a
+    end
+   b= new_array.join (" ")
+   b
+end 
