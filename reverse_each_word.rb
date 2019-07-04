b2a068506417eaefb847ddefdd057f44e0bf4947
Doug Kintop <dkintop@hotmail.com>
2019-07-04 01:03:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,13 @@
 def reverse_each_word(string)
   
-  string_array = ["test string"]
+  string_array = [ ]
   
-  string_array << string
- string_array
+  string_array
+  
+  
 
 
+
+  string_array
+  
 end 
